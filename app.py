dbb6312698ac7533748a23e6e71fc96f1c9b12f1
kidkender <84608369+Kidkender@users.noreply.github.com>
2024-01-11 14:55:04 +0700

    muti process

--- app.py
+++ app.py
@@ -3,84 +3,79 @@ import argparse
 import generate_image as generate_image
 import error.argument_parse as exception
 from enumeration.blur_enum import list_type
-from handle.argument.param_cli import process_brightness
 
+if __name__ == '__main__':
 
-parse = argparse.ArgumentParser(prog='SPARKMINDS',
-                                description='''Apply new feature to an image. 
-                                Features: rotate, resize, flip, noise, brightness, blur.''',
-                                epilog='''Thank you.''')
+    parse = argparse.ArgumentParser(prog='SPARKMINDS',
+                                    description='''Apply new feature to an image. 
+                                    Features: rotate, resize, flip, noise, brightness, blur.''',
+                                    epilog='''Thank you.''')
 
-exception.ErrorCatchingArgumentParser(parse)
+    exception.ErrorCatchingArgumentParser(parse)
 
-parse.add_argument('input_path', help='Path to the input image')
+    parse.add_argument('input_path', help='Path to the input image')
 
-parse.add_argument('output_path', help='Path to the output image')
+    parse.add_argument('output_path', help='Path to the output image')
 
+    parse.add_argument('limit', help='Limit image generate', default=5)
 
-parse.add_argument('limit', help='Limit image generate', default=5)
+    # Options
+    parse.add_argument('--resize', type=int, nargs='*', default=100,
+                       help='Resize percentage in range [10 - 200].')
+    parse.add_argument('--crop', type=int, nargs='?', default=100,
+                       help='Cropped image by percentage (default: no Change)')
 
-# Options
-parse.add_argument('--resize', type=int, nargs='*', default=100,
-                   help='Resize percentage in range [10 - 200].')
-parse.add_argument('--crop', type=int, nargs='?', default=100,
-                   help='Cropped image by percentage (default: no Change)')
+    parse.add_argument('--rotation', type=int, nargs='*', default=0,
+                       help='Rotation angle [0-360].')
 
-parse.add_argument('--rotation', type=int, nargs='*', default=0,
-                   help='Rotation angle [0-360].')
+    parse.add_argument('--flip-horizontal',
+                       action=argparse.BooleanOptionalAction, help='Flip images horizontally.')
+    parse.add_argument('--flip-vertical',
+                       action=argparse.BooleanOptionalAction, help='Flip images vertically.')
 
-parse.add_argument('--flip-horizontal',
-                   action=argparse.BooleanOptionalAction, help='Flip images horizontally.')
-parse.add_argument('--flip-vertical',
-                   action=argparse.BooleanOptionalAction, help='Flip images vertically.')
+    parse.add_argument('--constrast', nargs='*', type=int, default=None,
+                       help='Constrast images in range [0 - 100]')
 
-parse.add_argument('--constrast', nargs='*', type=int, default=None,
-                   help='Constrast images in range [0 - 100]')
+    parse.add_argument('--brightness', nargs='*', default=None, type=int,
+                       help='Brightness of images in the range [0 - 100]')
 
+    parse.add_argument('--noise', type=int, nargs='*', default=0,
+                       help='Level noise in range [0 - 100]')
+    parse.add_argument('--blur', nargs='?', default='Gaussian',
+                       help='Blur type in list [Gaussian, Median, Average]', choices=list_type())
+    parse.add_argument('--kn', type=int, default=5,
+                       help='Kernel blur default = 5')
 
-parse.add_argument('--brightness', nargs='*', default=None, type=int,
-                   help='Brightness of images in the range [0 - 100]')
+    args = parse.parse_args()
 
+    """_summary_
+        param required:
+            input_path, output_path, limit
+    """
+    input_path = args.input_path
+    output_path = args.output_path
+    limit = args.limit
 
-parse.add_argument('--noise', type=int, nargs='*', default=0,
-                   help='Level noise in range [0 - 100]')
-parse.add_argument('--blur', nargs='?', default='Gaussian',
-                   help='Blur type in list [Gaussian, Median, Average]', choices=list_type())
-parse.add_argument('--kn', type=int, default=5,
-                   help='Kernel blur default = 5')
+    resize_percentage = args.resize
+    crop_auto = args.crop
+    rotation_angle = args.rotation
 
+    constrast = args.constrast
 
-args = parse.parse_args()
+    brightness = args.brightness
 
-"""_summary_
-    param required:
-        input_path, output_path, limit
-"""
-input_path = args.input_path
-output_path = args.output_path
-limit = args.limit
+    flip_horizontal = args.flip_horizontal
+    flip_vertical = args.flip_vertical
+    noise = args.noise
 
-resize_percentage = args.resize
-crop_auto = args.crop
-rotation_angle = args.rotation
+    blur_type = args.blur
+    kernel = args.kn
 
-constrast = args.constrast
-
-brightness = args.brightness
-
-flip_horizontal = args.flip_horizontal
-flip_vertical = args.flip_vertical
-noise = args.noise
-
-blur_type = args.blur
-kernel = args.kn
-
-
-generate_image.generate(input_path=input_path, output_path=output_path,
-                        horizontal=flip_horizontal, vertical=flip_vertical,
-                        blur_type=blur_type, max_kernel=kernel,
-                        noise_max_level=noise,
-                        crop=crop_auto,
-                        brightness=brightness, constrast=constrast,
-                        max_percentage=resize_percentage, max_angle=rotation_angle,
-                        limit=limit)
+    generate_image.generate(input_path=input_path, output_path=output_path,
+                            horizontal=flip_horizontal, vertical=flip_vertical,
+                            blur_type=blur_type, max_kernel=kernel,
+                            noise_max_level=noise,
+                            crop=crop_auto,
+                            brightness=brightness, constrast=constrast,
+                            max_percentage=resize_percentage, max_angle=rotation_angle,
+                            limit=limit)
--- generate_image.py
+++ generate_image.py
@@ -9,51 +9,109 @@ from utils import value_util
 import convert_compress
 import numpy as np
 from handle.color import apply_change_color, apply_contrast
+import concurrent.futures
+import time
+
+start_time = time.time()
 
 
-def generate(*self, input_path, output_path,
-             max_percentage,
-             crop,
-             max_angle,
-             constrast, brightness,
-             horizontal=False, vertical=False,
-             noise_max_level, blur_type=None, max_kernel,
-             limit=1):
+# def generate(*self, input_path, output_path,
+#              max_percentage,
+#              crop,
+#              max_angle,
+#              constrast, brightness,
+#              horizontal=False, vertical=False,
+#              noise_max_level, blur_type=None, max_kernel,
+#              limit=1):
+#     if not os.path.exists(input_path):
+#         raise Exception("Folder not exist")
+
+#     for images in os.listdir(input_path):
+
+#         if not images.endswith((".png", ".jpg", ".jpeg")):
+#             continue
+#         image_path = os.path.join(input_path, images)
+
+#         img = cv.imread(image_path)
+#         if img is None:
+#             raise Exception("Img invalid")
+
+#         limit = value_util.get_number_from_str(limit)
+
+#         for x in range(limit):
+
+#             changed_crop = cr.apply_crop(img, percentage=crop)
+#             resized_img = rs.apply_resize(
+#                 changed_crop.copy(), max_percentage=max_percentage)
+
+#             rotation_img = rotation.apply_rotation(
+#                 resized_img, angle=max_angle)
+#             flipped_img = rotation.apply_flip(
+#                 rotation_img, horizontal, vertical)
+
+#             changed_brightness = br.apply_brightness(
+#                 flipped_img, alpha=constrast, beta=brightness)
+
+#             noise_img = nb.apply_noise(
+#                 changed_brightness, max_level=noise_max_level)
+#             blurred_image = nb.apply_blur(
+#                 noise_img, blur_type=blur_type, max_kernel=max_kernel)
+
+#             changed_color = np.array(apply_change_color(blurred_image))
+#             changed_contrast = np.array(apply_contrast(changed_color))
+#             convert_compress.convert_and_compress(
+#                 changed_contrast, output_path)
+#     calculate_time()
+
+
+def process_image(image_path, output_path, max_percentage, crop, max_angle, constrast, brightness,
+                  horizontal, vertical, noise_max_level, blur_type, max_kernel, limit):
+    img = cv.imread(image_path)
+    if img is None:
+        raise Exception(f"Invalid image: {image_path}")
+
+    limit = value_util.get_number_from_str(limit)
+
+    for x in range(limit):
+        changed_crop = cr.apply_crop(img, percentage=crop)
+        resized_img = rs.apply_resize(
+            changed_crop.copy(), max_percentage=max_percentage)
+        rotation_img = rotation.apply_rotation(resized_img, angle=max_angle)
+        flipped_img = rotation.apply_flip(rotation_img, horizontal, vertical)
+        changed_brightness = br.apply_brightness(
+            flipped_img, alpha=constrast, beta=brightness)
+        noise_img = nb.apply_noise(
+            changed_brightness, max_level=noise_max_level)
+        blurred_image = nb.apply_blur(
+            noise_img, blur_type=blur_type, max_kernel=max_kernel)
+        changed_color = np.array(apply_change_color(blurred_image))
+        changed_contrast = np.array(apply_contrast(changed_color))
+        convert_compress.convert_and_compress(changed_contrast, output_path)
+
+
+def generate(input_path, output_path, max_percentage, crop, max_angle, constrast, brightness,
+             horizontal=False, vertical=False, noise_max_level=0, blur_type=None, max_kernel=1, limit=1, batch_size=10):
     if not os.path.exists(input_path):
         raise Exception("Folder not exist")
 
-    for images in os.listdir(input_path):
+    image_paths = [os.path.join(input_path, image) for image in os.listdir(input_path)
+                   if image.lower().endswith((".png", ".jpg", ".jpeg"))]
 
-        if not images.endswith((".png", ".jpg", ".jpeg")):
-            continue
-        image_path = os.path.join(input_path, images)
+    limit = value_util.get_number_from_str(limit)
 
-        img = cv.imread(image_path)
-        if img is None:
-            raise Exception("Img invalid")
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for i in range(0, len(image_paths), batch_size):
+            batch_paths = image_paths[i:i + batch_size]
 
-        limit = value_util.get_number_from_str(limit)
+            futures = []
+            for image_path in batch_paths:
+                futures.append(executor.submit(process_image, image_path, output_path, max_percentage, crop,
+                                               max_angle, constrast, brightness, horizontal, vertical,
+                                               noise_max_level, blur_type, max_kernel, limit))
 
-        for x in range(limit):
+            concurrent.futures.wait(futures)
+    calculate_time()
 
-            changed_crop = cr.apply_crop(img, percentage=crop)
-            resized_img = rs.apply_resize(
-                changed_crop.copy(), max_percentage=max_percentage)
 
-            rotation_img = rotation.apply_rotation(
-                resized_img, angle=max_angle)
-            flipped_img = rotation.apply_flip(
-                rotation_img, horizontal, vertical)
-
-            changed_brightness = br.apply_brightness(
-                flipped_img, alpha=constrast, beta=brightness)
-
-            noise_img = nb.apply_noise(
-                changed_brightness, max_level=noise_max_level)
-            blurred_image = nb.apply_blur(
-                noise_img, blur_type=blur_type, max_kernel=max_kernel)
-
-            changed_color = np.array(apply_change_color(blurred_image))
-            changed_contrast = np.array(apply_contrast(changed_color))
-            convert_compress.convert_and_compress(
-                changed_contrast, output_path)
+def calculate_time():
+    print("-----%s seconds ----" % (time.time() - start_time))
